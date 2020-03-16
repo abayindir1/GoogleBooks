88d4f190e27691ee8d799282432d0faf75dc29e7
Abdullah Bayindir <byndrabdullah@gmail.com>
2020-03-16 16:06:14 -0500

    book model

---
+++ models/Book.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+    title:{
+        type:String,
+        required: true
+    },
+    authors:{
+        type:[String],
+        required: true,
+    },
+    link:{
+        type:String,
+        required: true
+    },
+    description:{
+        type:String,
+        required:true,
+    },
+    image:{
+        type:String,
+        required: true
+    },
+    googleId:{
+        type:String,
+        required:true,
+        unique:true
+    }
+})
+
+module.exports = mongoose.model("Book", bookSchema)
