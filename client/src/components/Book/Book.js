39b0b2338d50ec3158053c860030f8f2f3ac1ec8
Abdullah Bayindir <byndrabdullah@gmail.com>
2020-03-17 19:26:44 -0500

    book styles

--- client/src/components/Book/Book.js
+++ client/src/components/Book/Book.js
@@ -4,12 +4,16 @@ import "./style.css";
 function Item (props){
     return(
         <div className="book">
-            <h1>{props.title}</h1>
-            <h3>{props.author}</h3>
-            <img src={props.image} alt={props.title}/>
-            <p>{props.synopsis}</p>
-            <a href={props.link}>See the book</a>
-            <button onClick={props.click} id={props.id}>Save the book</button>
+            <h1 className="bookTitle">{props.title}</h1>
+            <h3 className="bookAuthor">{props.author}</h3>
+            <div className="bookContent">
+            <img src={props.image} alt={props.title} className="bookImage"/>
+            <p className="bookDescription">{props.synopsis}</p>
+            </div>
+            <div className="bookLinks">
+            <a href={props.link} className="bookSource">See the book</a>
+            <button onClick={props.click} id={props.id} className="saveButton">Save the book</button>
+            </div>
         </div>
     )
 }
