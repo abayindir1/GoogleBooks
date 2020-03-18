7e2bba3d813fc0e2204592b271ada22041e59183
Abdullah Bayindir <byndrabdullah@gmail.com>
2020-03-17 19:30:29 -0500

    Apis

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,15 +1,19 @@
 import axios from "axios";
 
 export default {
+  // get books from google api
   getGoogleBooks: function (q) {
-    return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + q)
+    return axios.get("/api/google", {params: {q: "title:" +q}});
   },
+  // gets all saved books
   getSavedBooks: function () {
     return axios.get("/api/books");
   },
+  // Saves a book to the database
   saveBook: function (savedBook) {
     return axios.post("/api/books", savedBook);
   },
+  // delete a book with the given id
   deleteBook: function (id) {
     return axios.delete("/api/books/" + id);
   }
