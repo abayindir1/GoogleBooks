99b066a2c37ed9bdab7e7ee4da1eb5d5340aeff4
Abdullah Bayindir <byndrabdullah@gmail.com>
2020-03-18 02:41:42 -0500

    heroku

--- server.js
+++ server.js
@@ -8,9 +8,15 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
+
 app.use(routes)
 
-mongoose.connect("mongodb://localhost/googlebooksdb");
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooksdb";
+mongoose.connect(MONGODB_URI);
+
 
 mongoose.connection.on("open", ()=>{
     console.log("Connection made")
