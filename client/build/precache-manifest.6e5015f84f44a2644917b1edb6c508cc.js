4a362908a66041587ba1ab86355d8a2a465ee75b
Abdullah Bayindir <byndrabdullah@gmail.com>
2020-03-19 17:11:06 -0500

    added build

---
+++ client/build/precache-manifest.6e5015f84f44a2644917b1edb6c508cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2ed264f944209a3f3026efaa766895",
+    "url": "/static/media/books.ee2ed264.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c653ae730209e74b9bf8",
+    "url": "/static/js/main.a3c5cbd8.chunk.js"
+  },
+  {
+    "revision": "b9fc260b72d61464cb2a",
+    "url": "/static/js/2.33b6f0d7.chunk.js"
+  },
+  {
+    "revision": "c653ae730209e74b9bf8",
+    "url": "/static/css/main.45795bba.chunk.css"
+  },
+  {
+    "revision": "095770dcaf6f90cfc90aab877e5dce4f",
+    "url": "/index.html"
+  }
+];
